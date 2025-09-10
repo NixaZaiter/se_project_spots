4b856024bd65ddafca29160021c1edaa17d0e8b0
Nixa Zaiter <linkclone30@gmail.com>
2025-09-09 19:50:50 -0500

    Add README and comment text labels

--- README.md
+++ README.md
@@ -1,24 +1,24 @@
-# Project 3: Spots
+# Project 3: Spots (Stage 5)
 
 ### Overview
 
 - Description & Fundamentals
 - Technology & Techniques
 - GitHub Pages
-- Video link
+- Notes
 
 **Description & Fundamentals**
 
-My submission for the second stage of the Spots project for sprint 3. The page should function as expected and be reactive to different screen sizes.
+My submission for the fifth stage of the Spots project for sprint 4. The modals should be able to be filled out and smoothly transition between open and closed.
 
 **Technology & Techniques**
 
-Most of the page consists of various grids with most elements having flexable values. Aspect ratio was used instead of the bottom padding trick for the images due to it's simplicity. One instance of flex was used for the header just to push the logo into position.
+Modal button and pop-up variables are kept near the top as a general grouping while the variables for input control are just below and seperated by which modal they go to. I've also decided to include some comment text in both the HTML and JS, just to help divide up some of the segments a bit better.
 
 **GitHub Pages**
 
 https://nixazaiter.github.io/se_project_spots/
 
-**Video link**
+**Notes**
 
-https://www.loom.com/share/f54f9c780fac4cfda5bc3b68ab57089e?sid=dd8dd13e-b203-4423-858f-a599f69ac245
+Struggled a bit with getting the inputs to update, but when asking Dot, it became quite obvious where I made a mistake. The rest was fairly straight forward.
--- scripts/index.js
+++ scripts/index.js
@@ -1,38 +1,44 @@
+// General Modal Setup and Buttons
 const editProfileBtn = document.querySelector(".profile__button_type_edit");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileClose = editProfileModal.querySelector(".modal__close");
 const profile = document.querySelector(".profile");
 const editProfileName = profile.querySelector(".profile__name");
 const editProfileDescription = profile.querySelector(".profile__description");
+const newPostBtn = document.querySelector(".profile__button_type_post");
+const newPostModal = document.querySelector("#new-post-modal");
+const newPostClose = newPostModal.querySelector(".modal__close");
+
+// Profile Modal Inputs
 const editProfileForm = editProfileModal.querySelector(".modal__content");
 const inputName = editProfileModal.querySelector("#input-name");
 const inputDescription = editProfileModal.querySelector("#input-description");
 
-const newPostBtn = document.querySelector(".profile__button_type_post");
-const newPostModal = document.querySelector("#new-post-modal");
-const newPostClose = newPostModal.querySelector(".modal__close");
-const addCardForm = newPostModal.querySelector(".modal__content");
+// Post Modal Inputs
+const newPostForm = newPostModal.querySelector(".modal__content");
 const inputImg = newPostModal.querySelector("#input-img");
 const inputCaption = newPostModal.querySelector("#input-caption");
 
+// Modal Open/Close Listeners + Current Profile Inputs
 editProfileBtn.addEventListener("click", function (evt) {
-  editProfileModal.classList.add("modal_is-open"); //open profile modal
-  inputName.value = editProfileName.textContent; //get current name
-  inputDescription.value = editProfileDescription.textContent; //get current description
+  editProfileModal.classList.add("modal_is-open");
+  inputName.value = editProfileName.textContent;
+  inputDescription.value = editProfileDescription.textContent;
 });
 
 editProfileClose.addEventListener("click", function (evt) {
-  editProfileModal.classList.remove("modal_is-open"); //close profile modal
+  editProfileModal.classList.remove("modal_is-open");
 });
 
 newPostBtn.addEventListener("click", function (evt) {
-  newPostModal.classList.add("modal_is-open"); //open post modal
+  newPostModal.classList.add("modal_is-open");
 });
 
 newPostClose.addEventListener("click", function (evt) {
-  newPostModal.classList.remove("modal_is-open"); //close post modal
+  newPostModal.classList.remove("modal_is-open");
 });
 
+// Functions and Submits
 function handleProfileFormSubmit(event) {
   event.preventDefault();
 
@@ -58,4 +64,4 @@ function handleAddCardSubmit(event) {
   newPostModal.classList.remove("modal_is-open");
 }
 
-addCardForm.addEventListener("submit", handleAddCardSubmit);
+newPostForm.addEventListener("submit", handleAddCardSubmit);
